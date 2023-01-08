bff0a8aaac2d4b7317f956aaf1a26d8c303944c2
Mauro Chumpitaz Polino <mchumpitazp@gmail.com>
2023-01-08 17:51:29 -0500

    Init frontend

---
+++ fillers_frontend/src/components/MainComponent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// Components
+
+// Redux
+
+class Main extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {}
+
+    render() {
+        return {
+            
+        }
+    }
+}
+
+export default Main;
