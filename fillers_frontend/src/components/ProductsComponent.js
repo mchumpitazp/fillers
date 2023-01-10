738e55ab27a2a0d0bebc965b9a8ffa62033201b6
Mauro Chumpitaz Polino <mchumpitazp@gmail.com>
2023-01-10 17:11:37 -0500

    Initial Backend

--- fillers_frontend/src/components/ProductsComponent.js
+++ fillers_frontend/src/components/ProductsComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { baseUrl } from '../shared/baseUrl';
 
 function Products() {
     return(
@@ -34,7 +35,7 @@ function Products() {
                             </article>
                     </Col>
                     <Col md className='order-md-1 img-container p-0'>
-                        <img src={require('./tmp.png')} alt='tmp'/>
+                        <img src={baseUrl + '/images/shine.png'} alt='ha_shine'/>
                     </Col>
                     
                 </Row>
@@ -44,7 +45,7 @@ function Products() {
                             <article className='product-info'>
                                 <h5>FILLER</h5>
                                 <hr/>
-                                <h2><strong>HA Shine</strong></h2>
+                                <h2><strong>HA Shape</strong></h2>
                                 
                                 <h4 className='product-subtitle'>
                                     <span>WHAT IS IT</span>
@@ -64,10 +65,68 @@ function Products() {
                             </article>
                         </Col>
                     <Col md className='img-container p-0'>
-                        <img src={require('./tmp.png')} alt='tmp'/>
+                        <img src={baseUrl + '/images/shape.png'} alt='ha_shape'/>
                     </Col>
                 </Row>
+                
+                <Row className='product-container'>
+                    <Col md className='order-md-2 pt-0'>
+                            <article className='product-info'>
+                                <h5>FILLER</h5>
+                                <hr/>
+                                <h2><strong>HA Fine</strong></h2>
+                                
+                                <h4 className='product-subtitle'>
+                                    <span>WHAT IS IT</span>
+                                    It is a long establishe distracted by the readable
+                                </h4>
 
+                                <div>
+                                    <p>Description</p>
+                                    <hr/>
+                                    <p>Description</p>
+                                    <hr/>
+                                    <p>Description</p>
+                                    <hr/>
+                                    <p>Description</p>
+                                    <hr/>
+                                </div>
+                            </article>
+                    </Col>
+                    <Col md className='order-md-1 img-container p-0'>
+                        <img src={baseUrl + '/images/fine.png'} alt='ha_fine'/>
+                    </Col>
+                    
+                </Row>
+
+                <Row className='product-container'>
+                    <Col md>
+                            <article className='product-info'>
+                                <h5>FILLER</h5>
+                                <hr/>
+                                <h2><strong>HA Deep</strong></h2>
+                                
+                                <h4 className='product-subtitle'>
+                                    <span>WHAT IS IT</span>
+                                    It is a long establishe distracted by the readable
+                                </h4>
+
+                                <div>
+                                    <p>Description</p>
+                                    <hr/>
+                                    <p>Description</p>
+                                    <hr/>
+                                    <p>Description</p>
+                                    <hr/>
+                                    <p>Description</p>
+                                    <hr/>
+                                </div>
+                            </article>
+                        </Col>
+                    <Col md className='img-container p-0'>
+                        <img src={baseUrl + '/images/deep.png'} alt='ha_deep'/>
+                    </Col>
+                </Row>
             </Container>
         </section>
         
