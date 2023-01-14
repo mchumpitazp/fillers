da800b97a4f1856559b53d9186248429700f0f1a
Mauro Chumpitaz Polino <mchumpitazp@gmail.com>
2023-01-14 14:12:15 -0500

    Form Partner functional with Azure SQL

--- fillers_frontend/src/components/FormPartnerComponent.js
+++ fillers_frontend/src/components/FormPartnerComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Button, Col, Container, Row } from 'reactstrap';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Container, Row, Col } from 'reactstrap';
+import { Form, FormGroup, Label, Input, FormFeedback, Alert, Spinner } from 'reactstrap';
 import CountrySelect from 'react-bootstrap-country-select';
 import 'react-bootstrap-country-select/dist/react-bootstrap-country-select.css';
 import PhoneInput from 'react-phone-input-2';
@@ -11,19 +11,58 @@ function FormPartner() {
     const [name, setName] = React.useState('');
     const [email, setEmail] = React.useState('');
     const [telephone, setTelephone] = React.useState('');
-    const [country, setCountry] = React.useState('');
+    const [country, setCountry] = React.useState();
     const [message, setMessage] = React.useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const [nameVal, setNameVal] = React.useState(true);
+    const [emailVal, setEmailVal] = React.useState(true);
+    const [telephoneVal, setTelephoneVal] = React.useState(true);
+    const [countryVal, setCountryVal] = React.useState(true);
 
+    const [validation, setValidation] = React.useState(false);
+    const [success, setSuccess] = React.useState(false);
+    const [failed, setFailed] = React.useState(false);
+    const [spinnerOpen, setSpinnerOpen] = React.useState(false);
+
+    React.useEffect(() => {
+        if (validation) {
+            handleValidSubmit();
+            setValidation(false);
+        }
+    }, [validation]);
+    
+    React.useEffect(() => {
+        let timeout;
+        if (success) {
+            timeout = setTimeout(() => { setSuccess(false) }, 2000);
+        }
+        return () => clearTimeout(timeout);
+    }, [success]);
+
+    React.useEffect(() => {
+        let timeout;
+        if (failed) {
+            timeout = setTimeout(() => { setFailed(false) }, 2000);
+        }
+        return () => clearTimeout(timeout);
+    }, [failed]);
+
+    const clearInputs = () => {
+        setName('');
+        setEmail('');
+        setTelephone('+33');
+        setCountry('');
+        setMessage('');
+    };
+
+    const handleValidSubmit = async () => {
         const newPartner = {
             name: name,
             email: email,
             telephone: telephone,
-            country: country.name,
+            country: country?.name,
             message: message
-        }
+        };
 
         try {
             const res = await fetch(baseUrl + '/partners', {
@@ -48,25 +87,70 @@ function FormPartner() {
                 throw errmess;
             });
 
+            setSpinnerOpen(false);
+
             if (res.status === 200) {
-                console.log('POST DONE')
+                console.log('POST DONE');
+                clearInputs();
+                setSuccess(true);
             }
             else {
-                console.log('POST ERROR')
+                console.log('POST ERROR');
+                setFailed(true);
             }
 
         } catch (error) {
+            setSpinnerOpen(false);
             console.log('POST Error: ', error.message);
         }
+    };
+ 
+    const validateForm = () => {
+        let nameTmp = false;
+        let emailTmp = false;
+        let telephoneTmp = false;
+        let countryTmp = false;
 
-        alert(JSON.stringify(newPartner));
+        const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        
+        if (name.length > 0) {
+            nameTmp = true;
+            setNameVal(true);
+        } else { setNameVal(false); }
+
+        if (emailRegex.test(email)) {
+            emailTmp = true;
+            setEmailVal(true);
+        } else { setEmailVal(false); }
+
+        if (telephone.length > 6) {
+            telephoneTmp = true;
+            setTelephoneVal(true);
+        } else { setTelephoneVal(false); }
+        
+        if (country?.name?.length > 3) {
+            countryTmp = true;
+            setCountryVal(true);
+        } else { setCountryVal(false); }
+
+        return (nameTmp & emailTmp & telephoneTmp & countryTmp);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (validateForm()) {
+            setSpinnerOpen(true);
+            setValidation(true);
+        } else {
+            setValidation(false)
+        }
     }
 
     return(
         <section id='form-partner'>
             <Container >
                 
-
                 <Row id='form-container'>
                     <h4>Become <strong>a partner</strong></h4>
                     
@@ -76,42 +160,61 @@ function FormPartner() {
                     </Col>
 
                     <Col md id='form-form'>
-                        <Form onSubmit={handleSubmit}>
+                        <Form noValidate onSubmit={handleSubmit}>
 
                             <FormGroup>
-                                <Label for='name'>Name *</Label>
-                                <Input id="name" type='text'
+                                <Label for='name'>Name *</Label> 
+                                <Input id="name" type='text' required
                                     value={name}
-                                    onChange={e => setName(e.target.value)}/>
+                                    onChange={e => setName(e.target.value)}
+                                    onClick={() => setNameVal(true)}
+                                    invalid={nameVal===false}/>
+                                <FormFeedback>
+                                     Please provide a valid name
+                                </FormFeedback>
                             </FormGroup>
 
                             <FormGroup>
                                 <Label for="email">Email *</Label>
-                                <Input id="email" type='email'
+                                <Input id="email" type='email' required
                                     value={email}
-                                    onChange={e => setEmail(e.target.value)}/>
+                                    onChange={e => setEmail(e.target.value)}
+                                    onClick={() => setEmailVal(true)}
+                                    invalid={emailVal===false}/>
+                                <FormFeedback>
+                                    Please provide a valid email
+                                </FormFeedback>
                             </FormGroup>
 
                             <FormGroup>
                                 <Label for='telephone'>Telephone *</Label>
                                 <Row>
                                     <Col md>
-                                        <PhoneInput id="telephone"
+                                        <PhoneInput
                                             country={'fr'}
                                             countryCodeEditable={false}
                                             value={telephone}
-                                            onChange={setTelephone}/>
+                                            onChange={setTelephone}
+                                            onClick={() => setTelephoneVal(true)}/>
+                                        <Input hidden invalid={telephoneVal===false}/>
+                                        <FormFeedback>
+                                            Please provide a valid telephone
+                                        </FormFeedback>
                                     </Col>
                                     <Col md></Col>
                                 </Row>
                             </FormGroup>
 
-                            <FormGroup>
+                            <FormGroup onClick={() => setCountryVal(true)}>
                                 <Label for='country'>Country *</Label>
-                                <CountrySelect id="country"
+                                <CountrySelect
                                     placeholder=''
                                     value={country}
-                                    onChange={setCountry}/>
+                                    onChange={setCountry} />
+                                <Input hidden invalid={countryVal===false}/>
+                                <FormFeedback>
+                                    Please choose a country
+                                </FormFeedback>
                             </FormGroup>
 
                             <FormGroup>
@@ -120,6 +223,23 @@ function FormPartner() {
                                     value={message}
                                     onChange={e => setMessage(e.target.value)}/>
                             </FormGroup>
+
+                            <Alert isOpen={success} className='my-3 py-2'>
+                                <h6 className='text-center m-0' style={{fontSize: '0.75rem'}}>Thank you!</h6>
+                            </Alert>
+
+                            <Alert isOpen={failed} className='my-3 py-2' color='danger'>
+                                <h6 className='text-center m-0' style={{fontSize: '0.75rem'}}>Please, try later</h6>
+                            </Alert>
+
+                            {
+                                spinnerOpen &&
+                                <div className='d-flex justify-content-center my-2'>
+                                    <Spinner type='grow' style={{backgroundColor: '#E686B8'}}>
+                                        Loading ...
+                                    </Spinner>
+                                </div>
+                            }
 
                             <Row>
                                 <Col md className='col-md-8 m-auto mb-3'>
@@ -133,6 +253,7 @@ function FormPartner() {
                             </Row>
                             
                         </Form>
+
                     </Col>
                 </Row>
 
